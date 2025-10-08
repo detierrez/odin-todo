3ea17d10a93f7e551bafe09baaba26e313187cb8
detierrez <detierrez@gmail.com>
2025-10-08 13:43:57 -0300

    chore: Update eslint rule to target webpack.js files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,15 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ["src/**/*.{js,mjs,cjs}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
   },
+  {
+    files: ["webpack.*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  }
 ]);
